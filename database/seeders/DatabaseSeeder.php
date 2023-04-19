c12f2fdba8fc71426275f7e277718a37df169ac6
TGKElit <tgkelit@gmail.com>
2023-04-19 13:56:27 +0200

    /question endpoint working, and some seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +15,40 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'id' => 1,
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        DB::table('games')->insert([
+            [
+                'id' => 1,
+                'user_id' => 1,
+            ]
+        ]);
+
+        DB::table('questions')->insert([
+            [
+                'id' => 1,
+                'the_question' => 'Är det blått?',
+                'alt_1' => 'Det är blått',
+                'alt_2' => 'ja',
+                'alt_3' => 'nej',
+                'alt_4' => 'kanske',
+                'the_answer' => 4
+            ]
+        ]);
+
+        DB::table('games_questions')->insert([
+            [
+                'id' => 1,
+                'question_id' => 1,
+                'game_id' => 1,
+                'result' => 'undecided',
+                'is_current' => true,
+
+            ]
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +18,28 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/question', function (Request $request) {
+    $game = DB::table('games')->where('id', $request->game_id);
+    if ($game->pluck('user_id')->get(0) == null) {
+        return "Could not find game";
+    }
+
+
+    if ($request->api_key == DB::table('users')->where('id', $game->pluck('user_id'))->pluck('remember_token')->get(0)) {
+        $question = DB::table('questions')
+            ->select('the_question', 'alt_1', 'alt_2', 'alt_3', 'alt_4')
+            ->join('games_questions', 'questions.id', '=', 'games_questions.question_id')
+            ->join('games', 'games.id', '=', 'games_questions.game_id')
+            ->where('games.id', $game->pluck('id'))
+            ->where('games_questions.is_current', true)->get()[0];
+    } else {
+        return "Wrong API-Key";
+    }
+
+    $response = $question;
+    return response()->json($response);
+
+});
+
+
